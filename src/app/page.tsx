2fe6f437f2714fc37bb30bec3ee7b2bf02bd909c
mukeshcoutloot <mukesh@coutloot.com>
2023-10-09 18:08:38 +0530

    ui changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import Image from 'next/image';
 import React, { useState, useEffect } from 'react';
+import pageData from './data.json';
 
 const AboutSection = () => {
   return (
@@ -25,33 +26,34 @@ const AboutSection = () => {
 const ExperienceSection = () => {
   return (
     <div className='overflow-y-scroll grid gap-[20px] h-[60vh] overflow-visible'>
-      {[1, 2].map((e) => (
+      {pageData.expereince.map((e) => (
         <div
-          key={e}
-          className='detail-card p-[30px] border-l-4 border-[#E5CCFF]'
+          key={e.company}
+          className='detail-card p-[30px] border-l-4 border-[#E5CCFF] h-fit'
         >
           <p className='text-white text-[12px] font-semibold capitalize leading-[155%]'>
-            Nov 2019 - Apr 2021
+            {e.date}
           </p>
           <div className='flex items-center'>
             <h1 className='text-[20px] font-semibold text-white'>Styx media</h1>
             <h2 className='ml-[15px] text-[16px] font-semibold text-white'>
-              Software Developer
+              {e.position}
             </h2>
           </div>
           <div className='mt-[16px] text-[#9B7CBB]'>
             <p className='text-[14px] font-medium leading-[163%]'>
-              Styx media is Startup marketing-based company founded in 2018.{' '}
+              {e.discription}
             </p>
-            <ul className='list-inside'>
-              <li className='list-disc'>
-                Design software architecture and database from scratch to meet
-                client requirements.
-              </li>
+            <ul className='pl-[12px] list-disc'>
+              {e.details.map((d) => (
+                <li key={d} className='list-disc'>
+                  {d}
+                </li>
+              ))}
             </ul>
           </div>
           <div className='mt-[12px] grid grid-cols-3 gap-4'>
-            {['NodeJS', 1, 2, 3, 4, 5, 6].map((e) => (
+            {e.techUsed.map((e) => (
               <div key={e} className='bg-[#D9D9D940] w-fit'>
                 <p className=' px-[16px] py-[6px] text-[#FADCFF] font-semibold text-[14px]'>
                   {e}
@@ -67,35 +69,46 @@ const ExperienceSection = () => {
 const ProjectSection = () => {
   return (
     <div className='overflow-y-scroll grid gap-[20px] h-[60vh] overflow-visible'>
-      {[1, 2].map((e) => (
+      {pageData.project.map((e) => (
         <div
-          key={e}
+          key={e.name}
           className='detail-card p-[30px] border-l-4 border-[#E5CCFF] h-fit'
         >
           <div className='flex items-center'>
-            <h1 className='text-[20px] font-semibold text-white'>Styx media</h1>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='12'
-              height='12'
-              className='ml-[10px]'
-              viewBox='0 0 12 12'
-              fill='none'
-            >
-              <path d='M1 11L11 1M11 1H1M11 1V11' stroke='white' />
-            </svg>
+            <h1 className='text-[20px] font-semibold text-white'>{e.name}</h1>
+            {e.redirectUrl !== '' && (
+              <a href={e.redirectUrl}>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  width='12'
+                  height='12'
+                  className='ml-[10px]'
+                  viewBox='0 0 12 12'
+                  fill='none'
+                >
+                  <path d='M1 11L11 1M11 1H1M11 1V11' stroke='white' />
+                </svg>
+              </a>
+            )}
           </div>
           <div className='mt-[16px] text-[#9B7CBB]'>
             <p className='text-[14px] font-medium leading-[163%]'>
-              Styx media is Startup marketing-based company founded in 2018.{' '}
+              {e.discription}
             </p>
           </div>
           <img
+            src={e.image}
             className='h-[150px] pt-[16px]'
-            src='https://brittanychiang.com/_next/image?url=%2Fimages%2Fprojects%2Fcourse-card.png&w=640&q=75'
+            alt={e.name}
           />
+
+          {/* <img
+            className='h-[150px] pt-[16px]'
+            src='/images/novacutisclinics.png' // Path relative to the public folder
+            alt={e.name}
+          /> */}
           <div className='mt-[30px] grid grid-cols-3 gap-4'>
-            {['NodeJS', 1, 2, 3, 4, 5, 6].map((e) => (
+            {e.techUsed.map((e) => (
               <div key={e} className='bg-[#D9D9D940] w-fit'>
                 <p className=' px-[16px] py-[6px] text-[#FADCFF] font-semibold text-[14px]'>
                   {e}
@@ -109,14 +122,15 @@ const ProjectSection = () => {
   );
 };
 
-const navigations = [
-  { name: 'ABOUT', component: AboutSection },
-  { name: 'EXPERIENCE', component: ExperienceSection },
-  { name: 'PROJECTS', component: ProjectSection }
-];
-
 export default function Home() {
+  const navigations = [
+    { name: 'ABOUT', component: <AboutSection /> },
+    { name: 'EXPERIENCE', component: <ExperienceSection /> },
+    { name: 'PROJECTS', component: <ProjectSection /> }
+  ];
+
   const [navigator, setNavigator] = useState(navigations[0]);
+
   return (
     <main>
       <div className='header'>
@@ -129,18 +143,23 @@ export default function Home() {
           <div className='pr-[40px] flex '>
             <div className='bg-[#3C156D] rounded-bl-lg rounded-br-lg mr-[20px] h-fit'>
               <div className='pt-[50px]'>
-                <img
-                  className='w-[35px] h-[35px]  rounded-full'
-                  src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png'
-                />
+                <a href='https://github.com/mk1319' target='_blank'>
+                  <img
+                    className='w-[35px] h-[35px]  rounded-full'
+                    alt='github'
+                    src='https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png'
+                  />
+                </a>
               </div>
             </div>
             <div className='bg-[#3C156D] rounded-bl-lg rounded-br-lg h-fit'>
               <div className='pt-[50px]'>
-                <img
-                  className='w-[35px] h-[35px]  rounded-full'
-                  src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHLsXrjekg4uJZG7ZrvDt976yA2hEaJxScjbmJD8sS8PdBA7wogJozouj2pMyM106W_NA&usqp=CAU'
-                />
+                <a href='https://www.linkedin.com/in/mukesh-rajpurohit-39147016b/'>
+                  <img
+                    className='w-[35px] h-[35px]  rounded-full'
+                    src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSHLsXrjekg4uJZG7ZrvDt976yA2hEaJxScjbmJD8sS8PdBA7wogJozouj2pMyM106W_NA&usqp=CAU'
+                  />
+                </a>
               </div>
             </div>
           </div>
@@ -155,9 +174,8 @@ export default function Home() {
               onClick={() => {
                 setNavigator(e);
               }}
-              className={`text-${
-                navigator.name == e.name ? '[#ECD8FF]' : '[#5C4390]'
-              } text-[14px] font-semibold leading-normal`}
+              className={`text-[14px] font-semibold leading-normal select-none
+              ${navigator.name == e.name ? 'active' : 'in-active'}`}
             >
               {e.name}
             </p>
@@ -165,12 +183,15 @@ export default function Home() {
         </div>
       </div>
 
-      <div className='main-details mt-[22px]'>{navigator.component()}</div>
-      <div className=' absolute bottom-0 h-[50px] left-0 right-0'>
+      <div className='main-details mt-[22px]'>{navigator.component}</div>
+      <div className=' absolute bottom-0 left-0 right-0'>
         <div className='flex justify-center items-center'>
-          <p className='text-[#C7B7F8] font-poppins text-[15px] font-medium'>
+          <a
+            href='mailto:srankwork@gmail.com'
+            className='text-[#C7B7F8] font-poppins text-[15px] font-medium pb-[10px] pt-[10px]'
+          >
             srankwork@gmail.com
-          </p>
+          </a>
         </div>
       </div>
     </main>
